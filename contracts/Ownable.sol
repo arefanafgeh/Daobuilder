fa48431f8cbdff71c8874f48b201f85499a62f06
aref <aanafgeh@gmail.com>
2025-04-02 19:45:32 +0330

    Setting constructor payable , since our smart contract does not handle ETH and to save up to 200 gas units in deployment

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -8,7 +8,7 @@ contract Ownable {
         address indexed previousOwner,
         address indexed newOwner
     );
-    constructor() {
+    constructor() payable{
         _owner = msg.sender;
         emit OwnershipTransferred(address(0), _owner);
     }
